6a2833742107febc7d54356e2a4a3ecd05cb866f
Alissa Kennedy <32944341+AlissaKennedy@users.noreply.github.com>
2018-02-03 16:40:42 -0800

    fonts!
    fonts yay!

--- source/index.php
+++ source/index.php
@@ -5,5 +5,7 @@
 <h1>Twisted Light Trails</h1>
 <?php echo "PHP is working"; ?>
 
+<p>Lorem ipsum dolor amet tbh craft beer master cleanse fingerstache swag intelligentsia try-hard. Aesthetic fixie 3 wolf moon craft beer succulents enamel pin godard, yuccie forage gluten-free poutine semiotics tbh normcore church-key. Chartreuse forage humblebrag godard truffaut kickstarter subway tile, sartorial williamsburg microdosing. Mustache viral mumblecore pinterest, vice hell of deep v unicorn franzen williamsburg squid taiyaki cray skateboard. Shaman hella plaid, authentic celiac umami flannel freegan subway tile pop-up. Normcore humblebrag four loko, lo-fi tilde etsy lumbersexual chambray man braid.</p>
 
+<p>Aesthetic beard portland mustache hexagon thundercats. Franzen raclette whatever palo santo, jianbing sartorial brunch glossier twee intelligentsia irony. Four dollar toast post-ironic echo park, chartreuse distillery helvetica pork belly brunch four loko blue bottle palo santo stumptown bespoke. Portland vaporware everyday carry shoreditch, ennui salvia green juice lomo authentic slow-carb. Drinking vinegar fanny pack venmo salvia fixie cronut pok pok affogato schlitz, PBR&B knausgaard distillery poke celiac cray.</p>
 <?php include "partials/foot.php" ?>
