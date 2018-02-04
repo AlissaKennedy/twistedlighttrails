57abe45ebf2776af83d17b58602879fc5dd794a8
Alissa Kennedy <32944341+AlissaKennedy@users.noreply.github.com>
2018-02-04 09:33:08 -0800

    image caption, download link

--- source/index.php
+++ source/index.php
@@ -3,11 +3,11 @@
 
 <div class="product container">
   <div class="info">
-    <h2 class="title">LED Hoop Pattern of the Month</h2>
+    <h2 class="title">LED Hoop Pattern Download</h2>
     <h3 class="subtitle">Mardi Gras Beads</h3>
     <label for="terms-of-service">I Agree to Terms</label>
     <input id="terms-of-service" type="checkbox">
-    <a href="patterns/mardi-gras-beads.bmp" disabled
+    <a href="patterns/mardi-gras-beads.bmp" download="mardi-gras-beads.bmp"
        class="download button icon-download">Download Hoop Pattern!</a>
     <div class="terms">
       <p>
@@ -27,6 +27,7 @@
     </div>
   </div>
   <div class="media" style="background-image: url(media/mardi-gras-beads-hoop-pattern.jpg);">
+	  <div class="caption">Hoop Pattern on 33" Phoenix Hoop, 2016 Model.</div>
   </div>
 </div>
 
