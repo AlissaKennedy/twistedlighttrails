9026b9e90436c8f2e38c9113b6928b1e253ed201
Troy Alford <troy@troyalford.com>
2018-02-04 10:40:03 -0800

    Mobile Works. Desktop Screwed.

--- source/index.php
+++ source/index.php
@@ -2,29 +2,29 @@
 <?php include "partials/head.php" ?>
 
 <div class="product container">
-  <div class="info">
-    <h2 class="title">LED Hoop Pattern Download</h2>
-    <h3 class="subtitle">Mardi Gras Beads</h3>
-    <label for="terms-of-service">I Agree to Terms</label>
-    <input id="terms-of-service" type="checkbox">
-    <a href="patterns/mardi-gras-beads.bmp" download="mardi-gras-beads.bmp"
-       class="download button icon-download">Download Hoop Pattern!</a>
-    <div class="terms">
-      <p>
-        <b>Terms:</b>
-        Free for personal use, including paid performances; repackaging or reselling as part of another product is prohibited.
-        Share with whoever you like, but please let them know where you got it!
-      </p>
-      <p>
-        <b>#twistedlighttrails</b> - made with ♥ by
-        <a class="link" href="//misstwistedrose.com">Miss Twisted Rose</a>
-      </p>
-	  <p>
-		  <a href="//www.instagram.com/misstwistedrose" class="icon-instagram"></a>
-		  <a href="//www.facebook.com/misstwistedrose" class="icon-facebook"></a>
-		  <a href="//www.misstwistedrose.com" class="icon-globe"></a>
-	  </p>
+  <h2 class="title">LED Hoop Pattern Download</h2>
+  <h3 class="subtitle">Mardi Gras Beads</h3>
+  <div class="tos">
+    <label class="agree-label" for="agree">I Agree to Terms</label>
+    <input class="agree" id="agree" type="checkbox">
+    <div class="dl-button">
+      <a href="patterns/mardi-gras-beads.bmp" download="mardi-gras-beads.bmp"
+        class="download button">Download Hoop Pattern!</a>
     </div>
+    <div class="terms">
+      <b>Terms:</b>
+      Free for personal use, including paid performances; repackaging or reselling as part of another product is prohibited.
+      Share with whoever you like, but please let them know where you got it!
+    </div>
+  </div>
+  <div class="made-with-love">
+    <b>#twistedlighttrails</b> - made with ♥ by
+    <a class="link" href="//misstwistedrose.com">Miss Twisted Rose</a>
+  </div>
+  <div class="social-links">
+    <a href="//www.instagram.com/misstwistedrose" class="icon-instagram"></a>
+    <a href="//www.facebook.com/misstwistedrose" class="icon-facebook"></a>
+    <a href="//www.misstwistedrose.com" class="icon-globe"></a>
   </div>
   <div class="media" style="background-image: url(media/mardi-gras-beads-hoop-pattern.jpg);">
 	  <div class="caption">Hoop Pattern on 33" Phoenix Hoop, 2016 Model.</div>
--- source/partials/head.php
+++ source/partials/head.php
@@ -1,10 +1,12 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title><?= isset($title) ? $title : 'Shiny Lights' ?> :: Twisted Light Trails :: LED Hoop Patterns</title>
-<link href="css/style.css" rel="stylesheet" type="text/css">
-</head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> <!--320-->
+
+    <title><?= isset($title) ? $title : 'Shiny Lights' ?> :: Twisted Light Trails :: LED Hoop Patterns</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+  </head>
 <body>
 
 <header class="gradient-border">
