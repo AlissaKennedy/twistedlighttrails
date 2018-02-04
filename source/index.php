8c3ed4fea5007afd208b6a768cd29b99d5155ebf
Troy Alford <troy@troyalford.com>
2018-02-04 01:00:50 -0800

    Add terms of service; update colors

--- source/index.php
+++ source/index.php
@@ -9,6 +9,17 @@
     <input id="terms-of-service" type="checkbox">
     <a href="patterns/mardi-gras-beads.bmp" disabled
        class="download button icon-download">Download</a>
+    <div class="terms">
+      <p>
+        <b>Terms:</b>
+        Free for personal use, including paid performances; repackaging or reselling as part of another product is prohibited.
+        Share with whoever you like, but please let them know where you got it!
+      </p>
+      <p>
+        <b>#twistedlighttrails</b> - made with ♥ by
+        <a class="link" href="//misstwistedrose.com">Miss Twisted Rose</a>
+      </p>
+    </div>
   </div>
   <div class="media" style="background-image: url(media/mardi-gras-beads-hoop-pattern.jpg);">
   </div>
