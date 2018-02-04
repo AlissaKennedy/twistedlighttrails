be7d5b6e54987fd24ca347287dae0ea92ee0f9e3
Troy Alford <troy@troyalford.com>
2018-02-03 19:06:26 -0800

    Add initial layout

--- source/index.php
+++ source/index.php
@@ -1,13 +1,17 @@
 <?php $title = "Pattern of the Month" ?>
-
 <?php include "partials/head.php" ?>
 
-<h1>Twisted Light Trails</h1>
-<?php echo "PHP is working"; ?>
+<div class="product container">
+  <div class="info">
+    <h3>Pattern of the Month</h3>
+    <h4>Mardi Gras</h4>
+    <div class="terms-of-service">
+      <input type="checkbox" /> Terms of Service
+    </div>
+    <button class="download icon-download">Download</button>
+  </div>
+  <div class="video">
+  </div>
+</div>
 
-<p>Made with love by <a href="#">Miss Twisted Rose</a></p>
-
-<p>Lorem ipsum dolor amet tbh craft beer master cleanse fingerstache swag intelligentsia try-hard. Aesthetic fixie 3 wolf moon craft beer succulents enamel pin godard, yuccie forage gluten-free poutine semiotics tbh normcore church-key. Chartreuse forage humblebrag godard truffaut kickstarter subway tile, sartorial williamsburg microdosing. Mustache viral mumblecore pinterest, vice hell of deep v unicorn franzen williamsburg squid taiyaki cray skateboard. Shaman hella plaid, authentic celiac umami flannel freegan subway tile pop-up. Normcore humblebrag four loko, lo-fi tilde etsy lumbersexual chambray man braid.</p>
-
-<p>Aesthetic beard portland mustache hexagon thundercats. Franzen raclette whatever palo santo, jianbing sartorial brunch glossier twee intelligentsia irony. Four dollar toast post-ironic echo park, chartreuse distillery helvetica pork belly brunch four loko blue bottle palo santo stumptown bespoke. Portland vaporware everyday carry shoreditch, ennui salvia green juice lomo authentic slow-carb. Drinking vinegar fanny pack venmo salvia fixie cronut pok pok affogato schlitz, PBR&B knausgaard distillery poke celiac cray.</p>
 <?php include "partials/foot.php" ?>
