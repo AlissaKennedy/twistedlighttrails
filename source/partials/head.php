ef19ff17fdbeb96e5330e4d095be2c1777d599e8
Alissa Kennedy <32944341+AlissaKennedy@users.noreply.github.com>
2018-02-03 16:00:21 -0800

    border gradient on header
    gradient border in progress

--- source/partials/head.php
+++ source/partials/head.php
@@ -3,5 +3,10 @@
 <head>
 <meta charset="utf-8">
 <title><?= isset($title) ? $title : 'Shiny Lights' ?> :: Twisted Light Trails :: LED Hoop Patterns</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+
+<header class="gradient-border">
+<a href="#" title="home" class="brand">Twisted Light Trails</a>
+</header>
