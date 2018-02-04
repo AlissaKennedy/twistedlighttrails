fd6c6edbd028d32dea4a91eb228cf569499b3d92
Alissa Kennedy <32944341+AlissaKennedy@users.noreply.github.com>
2018-02-04 01:24:23 -0800

    happy button color, social icons

--- source/index.php
+++ source/index.php
@@ -5,10 +5,10 @@
   <div class="info">
     <h2 class="title">LED Hoop Pattern of the Month</h2>
     <h3 class="subtitle">Mardi Gras Beads</h3>
-    <label for="terms-of-service">Terms of Service</label>
+    <label for="terms-of-service">I Agree to Terms</label>
     <input id="terms-of-service" type="checkbox">
     <a href="patterns/mardi-gras-beads.bmp" disabled
-       class="download button icon-download">Download</a>
+       class="download button icon-download">Download Hoop Pattern!</a>
     <div class="terms">
       <p>
         <b>Terms:</b>
@@ -19,6 +19,11 @@
         <b>#twistedlighttrails</b> - made with ♥ by
         <a class="link" href="//misstwistedrose.com">Miss Twisted Rose</a>
       </p>
+	  <p>
+		  <a href="//www.instagram.com/misstwistedrose" class="icon-instagram"></a>
+		  <a href="//www.facebook.com/misstwistedrose" class="icon-facebook"></a>
+		  <a href="//www.misstwistedrose.com" class="icon-globe"></a>
+	  </p>
     </div>
   </div>
   <div class="media" style="background-image: url(media/mardi-gras-beads-hoop-pattern.jpg);">
